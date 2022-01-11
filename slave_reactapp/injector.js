bccd0a983871c18010c23999f361b244a1de02d0
Sander Dijkxhoorn <15117158+sanderDijkxhoorn@users.noreply.github.com>
2022-01-11 12:58:58 +0100

    Button added
    Added a manual show answer button

--- slave_reactapp/injector.js
+++ slave_reactapp/injector.js
@@ -1,7 +1,6 @@
 console.log("Injecting... (slave_reactapp)");
 
-chrome.storage.sync.get(
-  {
+chrome.storage.sync.get({
     slider_alles: true,
     slider_knop: false,
     slider_auto: true,
@@ -13,9 +12,18 @@ chrome.storage.sync.get(
 
     const script = document.createElement("script");
     script.type = "text/javascript";
-    script.src = chrome.extension.getURL("slave_reactapp/script.js");
-    script.setAttribute("slider-button", items.slider_knop);
-    script.setAttribute("slider-auto", items.slider_auto);
-    document.head.appendChild(script);
+    if (items.slider_knop) {
+      script.src = chrome.extension.getURL("slave_reactapp/button.js");
+      script.setAttribute("slider-button", items.slider_knop);
+      script.setAttribute("slider-auto", items.slider_auto);
+      document.head.appendChild(script);
+    } else {
+      if (items.slider_auto) {
+        script.src = chrome.extension.getURL("slave_reactapp/script.js");
+        script.setAttribute("slider-button", items.slider_knop);
+        script.setAttribute("slider-auto", items.slider_auto);
+        document.head.appendChild(script);
+      }
+    }
   }
 );
